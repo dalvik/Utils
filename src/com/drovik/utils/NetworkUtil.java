8de76265561e7cbb174a09ef064d89c2958618e7
dalvik <sky.xctc@163.com>
2013-01-13 19:24:20 +0800

    add judge network status

--- src/com/drovik/utils/NetworkUtil.java
+++ src/com/drovik/utils/NetworkUtil.java
@@ -11,6 +11,12 @@ import android.net.NetworkInfo;
 
 public class NetworkUtil {
 
+	public final static int TYPE_WIFI = 2;
+	
+	public final static int TYPE_CMNET = 2;
+	
+	public final static int TYPE_CMWAP = 3;
+	
 	public static boolean checkNetwokEnable(Context context) {
 		ConnectivityManager connectivityManager = (ConnectivityManager) context
 				.getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -21,9 +27,49 @@ public class NetworkUtil {
 		return false;
 	}
 
+	public boolean isMobileConnected(Context context) {
+		if (context != null) {
+			ConnectivityManager mConnectivityManager = (ConnectivityManager) context
+					.getSystemService(Context.CONNECTIVITY_SERVICE);
+			NetworkInfo mMobileNetworkInfo = mConnectivityManager
+					.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+			if (mMobileNetworkInfo != null) {
+				return mMobileNetworkInfo.isAvailable();
+			}
+		}
+		return false;
+	}
+
+	public boolean isWifiConnected(Context context) {
+		if (context != null) {
+			ConnectivityManager mConnectivityManager = (ConnectivityManager) context
+					.getSystemService(Context.CONNECTIVITY_SERVICE);
+			NetworkInfo mWiFiNetworkInfo = mConnectivityManager
+					.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+			if (mWiFiNetworkInfo != null) {
+				return mWiFiNetworkInfo.isAvailable();
+			}
+		}
+		return false;
+	}
+
+	public boolean isNetworkConnected(Context context) {
+		if (context != null) {
+			ConnectivityManager mConnectivityManager = (ConnectivityManager) context
+					.getSystemService(Context.CONNECTIVITY_SERVICE);
+			NetworkInfo mNetworkInfo = mConnectivityManager
+					.getActiveNetworkInfo();
+			if (mNetworkInfo != null) {
+				return mNetworkInfo.isAvailable();
+			}
+		}
+		return false;
+	}
+
 	public static String getLocalIpAddress() {
 		try {
-			for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+			for (Enumeration<NetworkInterface> en = NetworkInterface
+					.getNetworkInterfaces(); en.hasMoreElements();) {
 				NetworkInterface intf = en.nextElement();
 				for (Enumeration<InetAddress> enumIpAddr = intf
 						.getInetAddresses(); enumIpAddr.hasMoreElements();) {
@@ -34,8 +80,33 @@ public class NetworkUtil {
 				}
 			}
 		} catch (SocketException ex) {
-			//Log.e("WifiPreference IpAddress", ex.toString());
+			// Log.e("WifiPreference IpAddress", ex.toString());
 		}
 		return "";
 	}
+
+	
+	public static int getAPNType(Context context) {
+		int netType = -1;
+		ConnectivityManager connMgr = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+		if (networkInfo == null) {
+			return netType;
+		}
+		int nType = networkInfo.getType();
+		if (nType == ConnectivityManager.TYPE_MOBILE) {
+			if (networkInfo.getExtraInfo().toLowerCase().equals("cmnet")) {
+				netType = TYPE_CMNET;
+			} else {
+				netType = TYPE_CMWAP;
+			}
+		} else if (nType == ConnectivityManager.TYPE_WIFI) {
+			netType = TYPE_WIFI;
+
+		}
+		return netType;
+
+	}
+
 }
